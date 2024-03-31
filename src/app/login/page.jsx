fdcbd5c0cb33963728ca542059d9534bd76cd0b5
Shubham Kharade <shubham.kharade28@gmail.com>
2024-04-01 00:45:14 +0530

    Slight changes

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,25 +1,41 @@
+'use client';
+
 import Background from "../components/Background";
 import Navbar from "../components/Navbar";
 import Link from "next/link";
 import "./login.css"
 import Footer from "../components/Footer";
+import { useState } from "react";
 
 const Login = () => {
+    const [userid, setUserId] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = (e) =>{
+        e.preventDefault();
+
+        // Logic to handle login click
+    }
+
     return (
-        <main>
+        <main className="flex flex-col w-screen relative">
             <Background />
             <Navbar/>
-            <cards>
+            <cards className="h-4/5">
                 <card>
-                    <h1>LOGIN</h1>
-                        <input type="Email" placeholder="Phone Number or Email" required/>
+                    <h1 className="text-white">Login</h1>
+                    <input type="Email" placeholder="Phone Number or Email" required
+                        value={userid} onChange={(e) => setUserId(e.target.value)}
+                    />
                     {/* span section containg password input field and forgoit password text */}
                     <span>
-                        <input type="password" placeholder="Password" />
+                        <input type="password" placeholder="Password" 
+                            value={password} onChange={(e) => setPassword(e.target.value)}
+                        />
                         <Link href="/forgotpass" id="forgot">Forgot password?</Link>
                     </span>
                     {/* LOGIN button */}
-                    <button>LOGIN</button>
+                    <button onClick={handleLogin}>LOGIN</button>  
                     
                     {/* NEW User Signup */}
                     <p>New User? 
