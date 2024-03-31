284a7f1b0d058804753d8ff640b2e9d143c19be6
Rohit-R-Khawale <rohitrkhawale@gmail.com>
2024-03-31 23:29:21 +0530

    Completed Login Signup and partially Completed Book Appointment page/ Rohit

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,10 +1,33 @@
 import Background from "../components/Background";
+import Navbar from "../components/Navbar";
+import Link from "next/link";
+import "./login.css"
+import Footer from "../components/Footer";
 
 const Login = () => {
     return (
         <main>
             <Background />
-            
+            <Navbar/>
+            <cards>
+                <card>
+                    <h1>LOGIN</h1>
+                        <input type="Email" placeholder="Phone Number or Email" required/>
+                    {/* span section containg password input field and forgoit password text */}
+                    <span>
+                        <input type="password" placeholder="Password" />
+                        <Link href="/forgotpass" id="forgot">Forgot password?</Link>
+                    </span>
+                    {/* LOGIN button */}
+                    <button>LOGIN</button>
+                    
+                    {/* NEW User Signup */}
+                    <p>New User? 
+                        <Link href="/signup" id="signUp" value="Submit">Sign Up</Link>
+                    </p>
+                </card>
+            </cards>
+            <Footer/>
         </main>
     )
 }
--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -1,8 +1,107 @@
-
+import Background from "../components/Background";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import "./signup.css"
 const Signup = () => {
     return (
         <main>
-            <h1>Signup</h1>
+            <Background/>
+            <Navbar/>
+            {/* Thois the tag containing the card element */}
+            <cards>
+                {/* This is the Card */}
+                <form>
+                    {/* heading of the card */}
+                    <h1>Sigup to HealCare</h1>
+                    <section className="PersonalDetail">
+                        <h2>Personal Details</h2>
+                        <div className="inputs">
+                            <div className="name">
+                                <label htmlFor="Name">Name: </label>
+                                <input type="text" id="Name" required />
+                            </div>  
+                        
+                            <div className="oneLine">
+
+                                <div>
+                                    <label htmlFor="DOB">Date of Birth: </label>
+                                    <input type="Date" id="DOB" required/>
+                                </div>
+
+                                <div>
+                                    <label htmlFor="Gender">Gender:  </label>
+                                    <select name="Gender" id="Gender" required>
+                                        <option value="male">Male</option>
+                                        <option value="female">Female</option>
+                                        <option value="other">Other</option>
+                                    </select>
+                                </div>
+                            </div>
+
+                            <div className="oneLine">                                
+                                <div>
+                                    <label htmlFor="pNo">Phone No: </label>
+                                    <input type="Numbers" id="pNo" required/>
+                                </div>
+    
+                                <div>
+                                    <label htmlFor="Email">Email: </label>
+                                    <input type="email" id="Email" />
+                                </div>
+                            </div>
+
+                            <div className="address">
+                                <label htmlFor="Address">Address</label>
+                                <textarea name="Address" id="Address" cols="66" rows="3" required></textarea>
+                            </div>
+                        </div>
+
+                        
+
+                    </section>
+
+                    <section2>
+                        <h2>Medical Details</h2>
+                        <div className="Med_details">
+
+
+                            <div>
+                                <label htmlFor="BloodGrp">Blood Group: </label>
+                                <select name="BloodGrp" id="BloodGrp">
+                                    <option value="O-">O-</option>
+                                    <option value="O+">O+</option>
+                                    <option value="A-">A-</option>
+                                    <option value="A+">A+</option>
+                                    <option value="B-">B-</option>
+                                    <option value="B+">B+</option>
+                                    <option value="AB-">AB-</option>
+                                    <option value="AB+">AB+</option>
+                                </select>
+                            </div>
+
+
+                            <div>
+                                <label htmlFor="Weight">Weight: </label>
+                                <input type="Numbers" id="Weight"/>
+                            </div>
+
+
+                            <div>
+                                <label htmlFor="Height">Height:</label>
+                                <input type="Numbers" id="Height" required/>
+                            </div>
+
+
+                            <div>
+                                <label htmlFor="Allergies">Allergies: </label>
+                                <input type="text" id="Allergies" required/>
+                            </div>
+                        </div>
+                    </section2>
+                    <div className="signup"><button >Sign Up</button></div>
+                </form>
+            </cards>
+            <Footer/>
         </main>
     )
 }
