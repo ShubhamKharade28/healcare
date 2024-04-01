a5f25d90bfacb01004b5584d4b439c92f75b12fa
Vaishnavi <mankarv041@gmail.com>
2024-04-01 19:16:21 +0530

    Service page partially complete/vaishnavi

--- src/app/services/page.jsx
+++ src/app/services/page.jsx
@@ -1,20 +1,131 @@
-// import './services.css';
+import './services.css';
 import Image from 'next/image';
 import healCareLogo from '@/assets/images/healcare_logo.jpg';
+import LOGo from '@/assets/images/hospital-profile.jpeg';
 import Navbar from '../components/Navbar';
 import Background from '../components/Background';
 
+
+
 const Services = () => {
     return (
-        <main className="w-screen min-h-screen flex flex-col relative">
-            <Background />
-            <Navbar />
-            <section className="flex flex-col w-full mx-10">
-                <h2 className="text-3xl">Hospitals</h2>
-                <div>
+        <main>
+            <Navbar/>
+            <h5 id="heading"> Hospitals</h5>
+            <div class="tp">
+
+                <div class="hospital1">
+                    <div class="pic">
+                        <Image src={LOGo} />
+                    </div>
+                    <div class="info">
+                        <p>hdgfhsgf</p>
+                    </div>
 
                 </div>
-            </section>
+                <div class="hospital2">
+                    <div class="pic">
+                        <Image src={LOGo} />
+                    </div>
+                    <div class="info">
+                        <p>hdgfhsgf</p>
+                    </div>
+
+                </div>
+                <div class="hospital3">
+                    <div class="pic">
+                    <Image src={LOGo} />
+                    </div>
+                    <div class="info">
+                        <p>hdgfhsgf</p>
+                    </div>
+                </div>
+                <div class="btn">See All </div>
+            </div>
+
+
+
+
+            <h5 id="heading"> Drug Store</h5>
+            <div class="tp2">
+
+                <div class="hospital4">
+                    <div class="pic">
+                    <Image src={LOGo} />
+                    </div>
+                    <div class="info">
+
+                    </div>
+
+                </div>
+                <div class="hospital5">
+                    <div class="pic">
+                    <Image src={LOGo} />
+                    </div>
+                    <div class="info">
+                        <p>hdgfhsgf</p>
+                    </div>
+
+                </div>
+                <div class="hospital6">
+                    <div class="pic">
+                    <Image src={LOGo} />
+                    </div>
+                    <div class="info">
+                        <p>hdgfhsgf</p>
+                    </div>
+                </div>
+                <div class="btn">See All </div>
+            </div>
+
+
+
+            <h5 id="heading"> Labs</h5>
+            <div class="tp3">
+
+                <div class="hospital7">
+                    <div class="pic">
+                    <Image src={LOGo} />
+                    </div>
+                    <div class="info">
+
+                    </div>
+
+                </div>
+                <div class="hospital8">
+                    <div class="pic">
+                    <Image src={LOGo} />
+                    </div>
+                    <div class="info">
+                        <p>hdgfhsgf</p>
+                    </div>
+
+                </div>
+                <div class="hospital9">
+                    <div class="pic">
+                    <Image src={LOGo} />
+                    </div>
+                    <div class="info">
+                        <p>hdgfhsgf</p>
+                    </div>
+                </div>
+                <div class="btn">See All </div>
+            </div>
+            <hr /><br />
+
+
+            <h1 id='Other_Services'>Other Services</h1>
+            <div className='OtherServicesDivision'>
+
+                <div class='tp4'>
+                    <h2 className='AI'> AI Support</h2>
+                </div>
+
+
+                <div class='tp5'>
+                    <h2 className='Delivery'>Delivery Services</h2>
+                </div>
+            </div>
         </main>
     )
 }
