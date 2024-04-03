9fa0adbddec03cf35eacfa01edd64bd6ff643364
Shubham Kharade <shubham.kharade28@gmail.com>
2024-04-03 18:34:59 +0530

    Fixed build error: bookappointment.css not found

--- src/app/bookappointment/page.jsx
+++ src/app/bookappointment/page.jsx
@@ -1,7 +1,6 @@
 'use client';
 
 import React, { useState } from 'react'
-import "./bookappointment.css"
 import Navbar from '../components/Navbar/Navbar'
 import Background from '../components/Background'
 import Footer from '../components/Footer'
